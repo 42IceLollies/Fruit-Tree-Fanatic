9e637207cc1f06b29813fec6a70cede8a8f63d3a
SodiumNitrateIsNot-A-Number <102708394+SodiumNitrateIsNot-A-Number@users.noreply.github.com>
2022-04-02 16:15:40 -0700

    Update script.js
    Added save data functions and gameData object

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// hello
+const gameData = {
+	example: 4
+}
+
+// =============== save data functions ===============
+
+// saves the data object values to localStorage
+function saveData() {
+	for (const key in gameData) {
+  	localStorage.setItem(`${key}`, gameData[key]);
+  }
+}
+
+// pulls the info from localStorage when loading a saved game
+function retrieveData() {
+	for (const key in gameData) {
+  	gameData[`${key}`] = localStorage.getItem(`${key}`);
+  	gameData[`${key}`] = localStorage.getItem(`${key}`);
+  }
+}
+
+// clears the saved data from localStorage
+function clearData() {
+	for (const key in gameData) {
+  	localStorage.deleteItem(`${key}`);
+  }
+}
