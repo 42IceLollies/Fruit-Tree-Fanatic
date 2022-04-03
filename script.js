32a6e3f9e111e8c554f2b36c747ba1ff1c642f65
SodiumNitrateIsNot-A-Number <102708394+SodiumNitrateIsNot-A-Number@users.noreply.github.com>
2022-04-03 10:22:46 -0700

    Update script.js
    Added purchase functions, updated the retrieveData function and the gameData object.

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const gameData = {
 	growth: 1,
 	pH: 7.0,
 	bees: false,
-	treeType: undefined,
+	treeType: "unselected",
 	infested: false,
 	fertilizer: 0,
-	coins: 100,
-	grafted: false,
-	pruneNum: 0,
-	pruneMax: [0, 0, 3, 5, 5, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8],
-	baseFruit: [0, 40, 55, 80, 85, 90, 95, 100, 100, 105, 110, 110, 115, 120],
+	  coins: 100,
+	  grafted: false,
+	  pruneNum: 0,
+	  pruneMax: [0, 0, 3, 5, 5, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8],
+	  baseFruit: [0, 40, 55, 80, 85, 90, 95, 100, 100, 105, 110, 110, 115, 120],
+	  fruitYield: 0,
 }
 
 // =============== save data functions ===============
@@ -22,12 +23,22 @@ function saveData() {
 	}
 }
 
-// pulls the info from localStorage when loading a saved game
+// pulls the info from localStorage when loading a saved game, and converts to original types
 function retrieveData() {
+	const toNum = ['coins', 'fertilizer', 'growth', 'level', 'pH', 'pruneNum', 'fruitYield'];
+	const toArray = ['baseFruit', 'pruneMax'];
+	const toBoolean = ['bees', 'grafted', 'infested'];
 	for (const key in gameData) {
 		gameData[`${key}`] = localStorage.getItem(`${key}`);
 		gameData[`${key}`] = localStorage.getItem(`${key}`);
 	}
+	toNum.forEach(num => {gameData[`${num}`] = parseFloat(gameData[`${num}`]); });
+	toArray.forEach(array => {
+		const strArray = gameData[`${array}`].split(',');
+		const numArray = strArray.map(str => Number(str));
+		gameData[`${array}`] = numArray;
+	});
+	toBoolean.forEach(boolean => {gameData[`${boolean}`] = JSON.parse(gameData[`${boolean}`]); });
 }
 
 // clears the saved data from localStorage
@@ -52,4 +63,28 @@ function adjustPH(change) {
 		gameData.pH = Math.round(gameData.pH * 100) / 100;
 	}
 	saveData();
+}
+
+// =========== purchase functions ===========
+
+function buyFertilizer() {
+	if (gameData.coins >= 10) {
+  		gameData.coins -= 10;
+		gameData.fertilizer++;
+		adjustPH('-');
+	}
+}
+
+function buyLimestone() {
+	if (gameData.coins >= 10) {
+  		gameData.coins -= 10;
+		adjustPH('+');
+	}
+}
+
+function buyPrune() {
+	if (gameData.coins >= 15 && gameData.pruneNum < gameData.pruneMax[gameData.level - 1]) {
+  		gameData.coins -= 15;
+		gameData.pruneNum++;
+	}
 }
